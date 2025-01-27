0079db058aa1c12f5917ab7b3dd40c2bca30adce
givensuman <givensuman@duck.com>
2025-01-27 16:03:10 -0500

    basic form/server interaction

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { normalizeURL } from "ufo"
 
-export async function GET() {
-  return NextResponse.json({ message: "Hello World" });
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+  if (!body.url) {
+    return NextResponse.json("No URL provided in POST request", { status: 300 })
+  }
+
+  normalizeURL("yo")
+
+  return NextResponse.json(null, { status: 200 })
 }
---
+++ src/components/URLEntry.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { toast } from "~/hooks/use-toast"
+import { Button } from "~/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form"
+import { Input } from "~/components/ui/input"
+
+const FormSchema = z.object({
+  url: z.string().url({
+    message: "Please enter a valid URL",
+  }),
+})
+
+export function URLEntry() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    // defaultValues: {
+    //   url: "",
+    // },
+  })
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    const response = await fetch("/api", { method: "POST", body: JSON.stringify({ url: data.url }) }).then(res => res.json())
+    console.log(response)
+    // toast({
+    //   title: "You submitted the following values:",
+    //   description: (
+    //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+    //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+    //     </pre>
+    //   ),
+    // })
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default URLEntry
